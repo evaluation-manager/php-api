ca7378aadd109bb166b542c7d0369d00c3c10554
Tainamiranda2 <tainamirandaferreira@gmail.com>
2022-10-11 14:27:15 -0300

    con

---
+++ avaliacao/src/Controllers/GradesController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Source\Controllers;
+use Source\Models\GradesModel;
+use LandKit\Route\Route;
+
+class GradesController{
+  public function index(){
+    //metodo get
+   $grades = (new GradesModel())->select("*")->fetch(true); 
+   if(!$grades) {
+    http_response_code(404);
+    echo json_encode([
+        "status"=>404,
+        "mensagem"=>"Nenhuma nota foi encontrada"
+    ]);
+    exit;
+   }
+   
+
+   $json=array_map(function($grades){
+        return [
+    "id"=>$grades->id,
+    "theme_id"=>$grades->theme_id,
+    "localization"=>$grades->localization,
+    "grades"=>$grades->grades,
+    "data"=>$grades->data,
+        ];
+   },$grades);
+   echo json_encode($json);
+  }  //post ainda falta
+  public function store(){
+    //echo "Teste";
+    $bodyData=Route::getJsonData();
+    $grades=new GradesModel();
+    $grades->theme_id=($bodyData['theme_id']);
+    $grades->localization=($bodyData['localization']);
+    $grades->grades=($bodyData['grades']);
+
+    $grades->save();
+
+    if($grades->fail()){
+        http_response_code(400);
+        echo json_encode([
+            "status"=>400,
+            "mensagem"=>  $grades->fail()->getMessage()
+        ]);
+
+        exit;
+    }
+    http_response_code(402);
+
+    echo json_encode([
+        'id'=> $grades->id,
+        'theme_id'=>$grades->theme_id,
+        'localization'=>$grades->localization,
+        'grades'=>$grades->grades,
+        'data'=>$grades->data,
+    ]);   
+   
+  }
+}
